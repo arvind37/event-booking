cb2a575423d1fcb21ea7400f521f087f22d47c4b
Arvind <arvind.kumawat37@gmail.com>
2026-02-10 19:55:51 +0530

    updated searchResult

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { fetchEvents } from "../api/eventsApi";
 import BookingModal from "../components/BookingModal";
-import EventCarousel from "../components/EventCarousel";
 
 
 function SearchResults() {
